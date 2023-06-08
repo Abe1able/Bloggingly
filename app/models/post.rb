88c3d97a612d5e55f88db368c872e6867c8c87db
Abel <abel.workmail@gmail.com>
2023-06-08 22:16:58 +0300

    Create User model

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,9 @@ class Post < ApplicationRecord
 	def update_posts_counter
 		author.update(posts_counter: author.posts.count)
 	end
+
+	def five_recent_comments
+		comments.order(created_at: :desc).limit(5)
+	end
+
 end
