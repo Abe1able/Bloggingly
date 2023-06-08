65f46637d5ac4cab556114e2dabceb7f4b82b3bd
Abel <abel.workmail@gmail.com>
2023-06-08 22:08:17 +0300

    Create Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+	belongs_to :author, class_name: 'User'
+	has_many :likes
+	has_many :comments
+
+	after_save :update_post_counter
+
+
+	def update_posts_counter
+		author.update(posts_counter: author.posts.count)
+	end
 end
