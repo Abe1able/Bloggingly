58d6002c8d06bac06bcace407fe60b7de4023647
Abel <abel.workmail@gmail.com>
2023-06-08 22:25:20 +0300

    Fix linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,11 @@
 class Like < ApplicationRecord
-	belongs_to :author, class_name: 'User'
-	belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
 
-	after_save :update_likes_counter
-	after_destroy :update_likes_counter
+  after_save :update_likes_counter
+  after_destroy :update_likes_counter
 
-	def update_likes_counter
-			post.update(likes_counter: post.likes.count)
-	end
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,15 @@
 class Post < ApplicationRecord
-	belongs_to :author, class_name: 'User'
-	has_many :likes
-	has_many :comments
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
 
-	after_save :update_post_counter
+  after_save :update_post_counter
 
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
 
-	def update_posts_counter
-		author.update(posts_counter: author.posts.count)
-	end
-
-	def five_recent_comments
-		comments.order(created_at: :desc).limit(5)
-	end
-
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
