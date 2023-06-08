e992102bc9f98de2adf441d46292364fc320bfb2
Abel <abel.workmail@gmail.com>
2023-06-09 00:10:13 +0300

    Add validations for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+	validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, :views_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :update_post_counter
 
   def update_posts_counter
