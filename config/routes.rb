2ff3a3aeade97086bf984aac8f30fa9a055d7527
Abe1able <abel.workmail@gmail.com>
2023-06-12 12:53:51 +0300

    Create Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   get 'posts/index'
   get 'posts/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
