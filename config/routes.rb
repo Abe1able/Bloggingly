a0b86e4a388687d595e4a96961b6794619b264a6
Abe1able <abel.workmail@gmail.com>
2023-06-12 20:30:56 +0300

    Remove basic routes according to review

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'posts/index'
-  get 'posts/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
