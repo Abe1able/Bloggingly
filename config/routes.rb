bf9f4a2b671d50e41ef9fed48d627b3b2f35997d
Abe1able <abel.workmail@gmail.com>
2023-06-14 00:57:56 +0300

    Write tests for users_controller and posts_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :users, only: [:index, :show] do
+  #   resources :posts, only: [:index, :show]
+  # end
+
+  # get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
+  # get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
+  # get "/users/:id", to: "users#show", as: "user"
+  # get "/users", to: "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
