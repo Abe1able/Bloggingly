97b19e78b69c3dc70b5e136a6edb7645877d15bf
Abe1able <abel.workmail@gmail.com>
2023-06-17 03:29:49 +0300

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,7 @@ class PostsController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:user_id])
-   # @post = Post.find_by!(id: params[:id], user_id: params[:user_id])
-     @post = Post.find(params[:id])
-    
+    # @post = Post.find_by!(id: params[:id], user_id: params[:user_id])
+    @post = Post.find(params[:id])
   end
 end
